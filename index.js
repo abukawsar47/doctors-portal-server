6c3a7eb9f939c057f1e1af733b59c089f906e062
abukawsar47 <abukawsar47ak@gmail.com>
2022-05-17 02:28:46 +0600

    heroku

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
+
 const app = express();
 const port = process.env.PORT || 5000;
+
 
 app.use(cors());
 app.use(express.json());
